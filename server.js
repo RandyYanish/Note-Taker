7b40cd28bfd535e5126b84ab04bbfc759c46b5b6
RandyYanish <rtaylorfargo@gmail.com>
2023-05-17 17:55:40 -0700

    Procfile update

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
-
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // IF middleware:
 
