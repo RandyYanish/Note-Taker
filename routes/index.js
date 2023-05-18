d20055e5b036d5375b28746594aa0b9942bb8482
RandyYanish <rtaylorfargo@gmail.com>
2023-05-18 16:39:44 -0700

    fixed js issues for the note page

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 
 // Import modular routers for /notes
 const notesRouter = require('./notes')
-const diagnosticsRouter = require('./diagnostics');
 
 const app = express();
 
 app.use('/notes', notesRouter);
-app.use('/diagnostics', diagnosticsRouter);
 
 module.exports = app;
